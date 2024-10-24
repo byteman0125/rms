bda7d3892c9e56378abc521c81a4b1d4c3a726de
byteboss125 <byteboss125@gmail.com>
2024-10-24 12:49:27 -0400

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,12 @@ const nextConfig = {
         locale: false
       }
     ]
+  },
+  env: {
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_DOCS_URL: process.env.NEXT_PUBLIC_DOCS_URL,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   }
 }
 
